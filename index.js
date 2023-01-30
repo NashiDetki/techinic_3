568926af0dea98c8120ea692f3205bf861940d1c
NashiDetki <93508687+NashiDetki@users.noreply.github.com>
2023-01-30 15:54:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+// console.log("2 + 2")
+// console.log(1000 - 500)
+// console.log(20 * 50)
+// console.log(128 / 2)
+// console.log(3000 < 3001)
+// console.log(200000-(400+400-0))
+// let арбуз;
+// let дыня;
+// let ответ;
+// арбуз=6
+// дыня=3
+// ответ=арбуз - дыня;
+// console.log("на", ответ, 'кг') 
+// let тарелка1;
+// let тарелка2;
+// let ответ;
+// тарелка1=8;
+// тарелка2=тарелка1-3;
+// ответ=тарелка1 + тарелка2;
+// console.log('всего',ответ)
+// let прочитала;
+// let отгодала;
+// let ответ;
+// прочитала=8;
+// отгодала=5;
+// ответ=прочитала - отгодала;
+// console.log('осталось',ответ)
+// let аквриум1;
+// let аквриум2;
+// let ответ;
+// аквриум1=3;
+// аквриум2=аквриум1+3;
+// ответ=аквриум1 + аквриум2;
+// console.log('всего',ответ )
+// let обэд;
+// let ужэн;
+// let ответ;
+// обэд=4;
+// ужэн=5;
+// ответ=обэд + ужэн;
+// console.log('всего', ответ)
+// let бочонок
+// let бочонок2
+// let ответ
+// бочонок=9
+// бочонок2=4
+// ответ=бочонок-бочонок2
+// console.log('осталось', ответ)
+// let самолет;
+// самолет=6;
+// let ракета;
+// ракета=2;
+// console.log('самолет',самолет);
+// console.log('ракета', ракета);
+// ответ=самолет-ракета;
+// console.log('больше на' , ответ)
+// function sei(){
+// let hello;
+// hello='привет Арслан';
+// console.log(hello);
+// }
+
+
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+// sei();
+let image = document.querySelector('viewer_image')
+console.log(image);
+
+
+let link1 = document.querySelector('link1');
+console.log(link1);
+
+
+let link2 = document.querySelector('link2')
+console.log(link2)
+
+
+let link3 = document.querySelector('link3')
+console.log(link3)
+
+
+link1.addEventListener('click',function (){
+    image.srs = '/img/photo1669485977'
+})
